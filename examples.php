18a4647e5ce1f04a90432b467108cf47bb42f775
Vasiliy <vasiliy@bitcoin.co.th>
2019-03-26 18:40:11 +0700

    formatting

--- examples.php
+++ examples.php
@@ -10,10 +10,10 @@ try {
         ->url("https://httpbin.org/get")
         ->get();
 
-    echo "Status: " . $response->status() . "\n\n";
-    echo "Headers: " . var_export($response->headers(), true) . "\n\n";
-    echo "Body: " . $response->body() . "\n\n";
-    echo "Json: " . var_export($response->json(), true) . "\n\n";
+    echo "Status:   " . $response->status() . "\n\n";
+    echo "Headers:  " . var_export($response->headers(), true) . "\n\n";
+    echo "Body:     " . $response->body() . "\n\n";
+    echo "Json:     " . var_export($response->json(), true) . "\n\n";
 } catch (JttpException $e) {
     // it will catch all errors here including non 2xx response codes and json_decode errors
     echo "Error: {$e->getMessage()}\n";
